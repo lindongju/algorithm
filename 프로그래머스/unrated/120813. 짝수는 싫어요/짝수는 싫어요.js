84b73399405e917c6a6790565d9b494aca06f28a
DongJu <djlim91@naver.com>
2022-10-02 00:37:15 +0900

    [unrated] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,3 +1,3 @@
 function solution(n) {
-    return Array(n).fill(1).map((v,idx) => v + idx).filter(v => v % 2 != 0);
+    return Array(n).fill(1).map((v,idx) => v + idx).filter(v => v % 2 !== 0);
 }
