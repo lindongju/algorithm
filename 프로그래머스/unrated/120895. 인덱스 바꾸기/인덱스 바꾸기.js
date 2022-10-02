819082c3afbd277008d4d6c82437e0b647848478
DongJu <djlim91@naver.com>
2022-10-02 14:50:39 +0900

    [unrated] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, num1, num2) {
+    
+    let answer = [...my_string];
+
+    [answer[num1], answer[num2]] = [answer[num2], answer[num1]];
+    
+    return answer.join('');
+}
