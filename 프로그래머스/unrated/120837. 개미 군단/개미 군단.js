38573323d9249af66f4eef30f9e237a66fa3ec7e
DongJu <djlim91@naver.com>
2022-10-02 14:24:32 +0900

    [unrated] Title: 개미 군단, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,7 @@
 function solution(hp) {
     
     return [5, 3, 1].reduce((acc, attack) => {
-        const [totalHp, count] = acc; console.log(acc);
+        const [totalHp, count] = acc;
         const totalAttack = (totalHp - (totalHp % attack));
         const antCount = totalAttack / attack;
 
