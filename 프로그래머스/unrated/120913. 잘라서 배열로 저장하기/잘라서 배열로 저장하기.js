02478ea5b26df3d2a994532ff9f7e053b9d35ee1
DongJu <djlim91@naver.com>
2022-10-02 16:30:43 +0900

    [unrated] Title: 잘라서 배열로 저장하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,4 +1,6 @@
 function solution(my_str, n) {
     
-    return Array(Math.ceil(my_str.length / n)).fill(1).map((v,i) => my_str.substr(i*n, n))
+    //"abc1Addfggg4556b".match(/[A-Za-z0-9]{1,6}/g)
+    
+    return Array(Math.ceil(my_str.length / n)).fill(1).map((v,i) => my_str.substr(i*n, n));
 }
