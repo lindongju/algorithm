42e799f812420467643ffda4cf5d0085e50aaa40
DongJu <djlim91@naver.com>
2022-10-02 20:18:53 +0900

    [unrated] Title: 가까운 수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
@@ -1,4 +1,4 @@
 function solution(array, n) {
-    var arr = array.sort((a,b) => a-b).map( v => Math.abs(v - n));
+    var arr = array.map(v => Math.abs(v - n));
     return array[arr.indexOf(Math.min(...arr))];
 }
