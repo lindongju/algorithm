91c7adb8bd305537ffc62b66b018c8f81611cb7c
DongJu <djlim91@naver.com>
2022-10-02 19:32:19 +0900

    [unrated] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.js
@@ -1,5 +1,7 @@
 function solution(box, n) {
     
-    
+    // 직육면체 부피 ( 가로 * 세로 * 높이 )
+    // 정육면체 부피 ( 가로 * 세로 * 높이 )
+    // 직육면체 가로 세로 높이를 정육면체의 한면에 맞추고 나눈다.
    return box.map(v => v - (v%n)).reduce((a,b) => a * b) / Math.pow(n,3)
 }
