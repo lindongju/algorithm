413e7dc5f24e0caadc30d1d7a762dd8b5964e9c2
DongJu <djlim91@naver.com>
2022-10-02 17:59:34 +0900

    [unrated] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,5 +1,3 @@
 function solution(numbers) {
-  
-    //Math.max(...numbers.map((v,idx) =>  Math.max(...numbers.filter((dv, dIdx) => dIdx != idx).map(s => s * v) ) ))
-    return Math.max(...numbers.map((v,idx) =>  Math.max(...numbers.filter((dv, dIdx) => dIdx != idx).map(s => s * v) ) )) 
+     return Math.max(...numbers.map((v,idx) =>  Math.max(...numbers.filter((dv, dIdx) => dIdx != idx).map(s => s * v) ) )) 
 }
