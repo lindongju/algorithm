2373f8580fb6f098adaba04382670b7087bab597
DongJu <djlim91@naver.com>
2022-10-03 13:05:03 +0900

    [unrated] Title: 다음에 올 숫자, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,4 +1,20 @@
 function solution(common) {
-    var answer = 0;
-    return answer;
+    
+    let num1 = common[common.length-1];
+    let num2 = common[common.length-2];
+    let num3 = common[common.length-3];
+    let rslt = 0;
+    
+    if (num2 - num3 == num1 - num2) {
+        
+        rslt = num1 + (num1 - num2);
+    }
+    
+    if (num2 / num3 == num1 / num2) {
+        
+        rslt = num1 * (num1 / num2);
+    }
+    
+    return rslt;
+    
 }
